a567fca8f0ebeff94d00929d4623080d19cd53b0
Akash Bhowmick <akabhowmick@gmail.com>
2024-06-08 23:09:01 -0400

    first commit

---
+++ Week3/guessGame.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	target := rand.Intn(100) + 1
+
+	var guess int
+	attempts := 0
+	const maxAttempts = 10
+
+	for {
+		fmt.Print("Guess a number between 1 and 100: ")
+		fmt.Scanf("%d", &guess)
+
+		attempts++
+
+		if guess < target {
+			fmt.Println("Too low")
+		} else if guess > target {
+			fmt.Println("Too high")
+		} else {
+			break
+		}
+
+		if attempts == 10 {
+			break
+		}
+
+		if attempts != maxAttempts && attempts != 10 {
+			continue
+		} else {
+			break
+		}
+	}
+	fmt.Printf("\nTarget was %v ", target)
+	fmt.Printf("\nYou Guessed it in %v ", attempts)
+}
